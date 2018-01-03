e3bda35b8e7c564e094d57a3559f818ea8fd1536
brfoster <brfoster@cisco.com>
2018-01-03 12:43:33 -0500

    Added LSP's and cleanded up code.  Also added node coordinates.

--- waecode/planbuild.py
+++ waecode/planbuild.py
@@ -1,5 +1,7 @@
+import sys
 import com.cisco.wae.design
 from com.cisco.wae.design.model.net import HopType
+from com.cisco.wae.design.model.net import LSPType
 # keys
 from com.cisco.wae.design.model.net import NodeKey
 from com.cisco.wae.design.model.net import InterfaceKey
@@ -7,6 +9,12 @@ from com.cisco.wae.design.model.net.layer1 import L1NodeKey
 from com.cisco.wae.design.model.net.layer1 import L1PortKey
 from com.cisco.wae.design.model.net.layer1 import L1CircuitKey
 from com.cisco.wae.design.model.net.layer1 import L1CircuitPathKey
+from com.cisco.wae.design.model.net import DemandKey
+from com.cisco.wae.design.model.net import DemandEndpointKey
+from com.cisco.wae.design.model.net import ServiceClassKey
+from com.cisco.wae.design.model.net import LSPKey
+from com.cisco.wae.design.model.traffic import DemandTrafficKey
+from com.cisco.wae.design.model.net import TrafficLevelKey
 # records
 from com.cisco.wae.design.model.net import NodeRecord
 from com.cisco.wae.design.model.net.layer1 import L1NodeRecord
@@ -17,13 +25,18 @@ from com.cisco.wae.design.model.net.layer1 import L1CircuitPathRecord
 from com.cisco.wae.design.model.net.layer1 import L1CircuitPathHopRecord
 from com.cisco.wae.design.model.net import InterfaceRecord
 from com.cisco.wae.design.model.net import CircuitRecord
+from com.cisco.wae.design.model.net import DemandRecord
+from com.cisco.wae.design.model.net import ServiceClassRecord
+from com.cisco.wae.design.model.net import LSPRecord
 
 
 def generateL1nodes(plan, l1nodelist):
     l1NodeManager = plan.getNetwork().getL1Network().getL1NodeManager()
     for l1node in l1nodelist:
-        l1nodeRec = L1NodeRecord(name=l1node)
-        l1NodeManager.newL1Node(l1nodeRec)
+        l1nodeRec = L1NodeRecord(name=l1node['Name'])
+        newl1node = l1NodeManager.newL1Node(l1nodeRec)
+        newl1node.setLatitude(int(l1node['Y']))
+        newl1node.setLongitude(int(l1node['X']))
 
 
 def generateL1links(plan, l1linklist):
@@ -52,7 +65,7 @@ def generateL1circuit(plan, name, l1nodeA, l1nodeB, l1hops, bw):
     l1portAkey = L1PortKey(name=l1portAname, l1Node=l1nodeAKey)
     l1portBkey = L1PortKey(name=l1portBname, l1Node=l1nodeBKey)
 
-    l1circuitrec = L1CircuitRecord(name=name, l1PortAKey=l1portAkey, l1PortBKey=l1portBkey,bandwidth=bw)
+    l1circuitrec = L1CircuitRecord(name=name, l1PortAKey=l1portAkey, l1PortBKey=l1portBkey, bandwidth=bw)
 
     l1circuitManager = plan.getNetwork().getL1Network().getL1CircuitManager()
     l1circuit = l1circuitManager.newL1Circuit(l1circuitrec)
@@ -76,7 +89,8 @@ def generateL1circuit(plan, name, l1nodeA, l1nodeB, l1hops, bw):
             hoptype = HopType('PathStrict', 0)
         else:
             hoptype = HopType('PathLoose', 0)
-        l1hoprec = L1CircuitPathHopRecord(l1CircPathKey=l1circuitpath.getKey(), hopNode=L1NodeKey(l1hop),step=c,type=hoptype)
+        l1hoprec = L1CircuitPathHopRecord(l1CircPathKey=l1circuitpath.getKey(), hopNode=L1NodeKey(l1hop), step=c,
+                                          type=hoptype)
         l1circuitpath.addHop(l1hoprec)
         c += 1
 
@@ -85,15 +99,17 @@ def generateL1circuit(plan, name, l1nodeA, l1nodeB, l1hops, bw):
 
 def generateL3nodes(plan, l3nodelist):
     for l3node in l3nodelist:
-        nodeRec = NodeRecord(name=l3node)
-        plan.getNetwork().getNodeManager().newNode(nodeRec)
+        nodeRec = NodeRecord(name=l3node['Name'])
+        newl3node = plan.getNetwork().getNodeManager().newNode(nodeRec)
+        newl3node.setLatitude(int(l3node['Y']))
+        newl3node.setLongitude(int(l3node['X']))
 
 
-def generateL3circuit(plan,name,l3nodeA,l3nodeB):
+def generateL3circuit(plan, name, l3nodeA, l3nodeB):
     nodeAKey = NodeKey(l3nodeA)
     nodeBKey = NodeKey(l3nodeB)
-    nodeAintfname = "L3_intf_" + name + "_to_"+ l3nodeB
-    nodeBintfname = "L3_intf_" + name + "_to_"+ l3nodeA
+    nodeAintfname = "L3_intf_" + name + "_to_" + l3nodeB
+    nodeBintfname = "L3_intf_" + name + "_to_" + l3nodeA
     intfArec = InterfaceRecord(sourceKey=nodeAKey, name=nodeAintfname, isisLevel=2)
     intfBrec = InterfaceRecord(sourceKey=nodeBKey, name=nodeBintfname, isisLevel=2)
     circRec = CircuitRecord(name=name)
@@ -101,3 +117,70 @@ def generateL3circuit(plan,name,l3nodeA,l3nodeB):
     circuit = network.newConnection(ifaceARec=intfArec, ifaceBRec=intfBrec, circuitRec=circRec)
 
     return circuit
+
+def generate_lsps(plan,lsps,l3nodeloopbacks):
+    for lsp in lsps:
+        lspBW = int(int(lsp.get('signalled-bw'))/1000)
+        if lspBW > 0:
+            lspName = lsp.get('fdn').split('!')[1].split('=')[1]
+            demandName = "Demand for " + lspName
+            src=getnodename(lsp.get('Tunnel Source'),l3nodeloopbacks)
+            dest = getnodename(lsp.get('Tunnel Destination'), l3nodeloopbacks)
+            new_private_lsp(plan, src, dest, lspName)
+            new_demand_for_LSP(plan, src, dest, lspName, demandName, lspBW)
+
+
+def new_demand_for_LSP(id, src, dest, lspName, demandName, val):
+    serviceClass = 'Default'
+    serviceClassMgr = id.getNetwork().getServiceClassManager()
+    serviceClassExists = serviceClassMgr.hasServiceClass(
+        ServiceClassKey(name=serviceClass))
+    if not serviceClassExists:
+        serviceClassRecord = ServiceClassRecord(name=serviceClass)
+        serviceClassMgr.newServiceClass(serviceClassRecord)
+
+    keylist = serviceClassMgr.getAllServiceClassKeys()
+    dmdRec = DemandRecord(
+        name=demandName,
+        source=DemandEndpointKey(key=src),
+        destination=DemandEndpointKey(key=dest),
+        serviceClass=ServiceClassKey(name='Default'),
+        privateLSP=LSPKey(
+            name=lspName,
+            sourceKey=NodeKey(name=src)
+        )
+    )
+    dmdMgr = id.getNetwork().getDemandManager()
+    dmdMgr.newDemand(dmdRec)
+
+    dmdTraffKey = DemandTrafficKey(
+        traffLvlKey=TrafficLevelKey(name='Default'),
+        dmdKey=DemandKey(
+            name=demandName,
+            source=DemandEndpointKey(key=src),
+            destination=DemandEndpointKey(key=dest),
+            serviceClass=ServiceClassKey(name='Default'),
+        )
+    )
+    dmdTrafficMgr = id.getTrafficManager().getDemandTrafficManager()
+    dmdTrafficMgr.setTraffic(dmdTraffKey, val)
+
+
+def new_private_lsp(id, src, dest, name):
+    lspRec = LSPRecord(
+        sourceKey=NodeKey(name=src),
+        name=name,
+        destinationKey=NodeKey(name=dest),
+        isActive=True,
+        isPrivate=True,
+        type=LSPType.RSVP
+    )
+    lspMgr = id.getNetwork().getLSPManager()
+    lspMgr.newLSP(lspRec)
+
+
+def getnodename(loopback,nodelist):
+    for node in nodelist:
+        for k,v in node.items():
+            if v == loopback:
+                return k
