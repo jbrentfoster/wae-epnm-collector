aa74733aaf235eca6ca0fb250c7bd3a9f81e8260
brfoster <brfoster@cisco.com>
2018-01-03 12:45:04 -0500

    Added LSP's and cleanded up code.  Also added node coordinates.

--- wae_api.py
+++ wae_api.py
@@ -9,6 +9,7 @@ os.environ['PATH'] = newPATH
 import com.cisco.wae.design
 import waecode.planbuild
 import json
+import csv
 
 
 def main():
@@ -19,15 +20,29 @@ def main():
     fileName = os.path.join(cwd, 'planfiles/blank.pln')
     plan = conn.getPlanManager().newPlanFromFileSystem(fileName)
 
+    # Read node coordinates file into a dict
+    nodecoordinates = []
+    with open('waecode/node_coordinates.csv', 'rb') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            nodecoordinates.append(row)
+        f.close()
+
+    # Add L1 nodes to plan
     with open("jsonfiles/l1Nodes.json", 'rb') as f:
         l1nodesdict = json.load(f)
         f.close()
 
     l1nodes = []
     for k1, v1 in l1nodesdict.items():
-        l1nodes.append(v1['Name'])
+        for node in nodecoordinates:
+            if node['Node'] == v1['Name']:
+                tmpnode = {'Name': v1['Name'], 'X': node['X'], 'Y': node['Y']}
+        l1nodes.append(tmpnode)
 
     waecode.planbuild.generateL1nodes(plan, l1nodelist=l1nodes)
+
+    # Add L1 links to plan
 
     with open("jsonfiles/l1Links.json", 'rb') as f:
         l1linksdict = json.load(f)
@@ -39,16 +54,20 @@ def main():
 
     waecode.planbuild.generateL1links(plan, l1linklist=l1links)
 
+    # Add L3 nodes to plan
     with open("jsonfiles/l3Links_reordered_l1hops.json", 'rb') as f:
         l3linksdict = json.load(f)
         f.close()
 
     l3nodes = []
     for k1, v1 in l3linksdict.items():
-        l3nodes.append(k1)
+        for node in nodecoordinates:
+            if node['Node'] == k1:
+                tmpnode = {'Name': k1, 'X': node['X'], 'Y': node['Y']}
+        l3nodes.append(tmpnode)
+    waecode.planbuild.generateL3nodes(plan, l3nodelist=l3nodes)
 
-    waecode.planbuild.generateL3nodes(plan, l3nodes)
-
+    # Add L3 links to plan and stitch to L1 links where applicable
     c = 0
     i = 0
     linkslist = []
@@ -58,13 +77,11 @@ def main():
         for k2, v2 in v1.items():
             if isinstance(v2, dict):
                 for k3, v3 in v2.items():
-                    print "***************Linkname is: " + k3
-                    firstl1node = ""
-                    lastl1node = ""
-                    l1hops = []
+                    # print "***************Linkname is: " + k3
                     lastnode = v3.get('Neighbor')
                     discoveredname = v3.get('discoveredname')
                     rsvpbw = float(v3.get('RSVP BW').split(' ')[0])
+                    intfbw = getintfbw(rsvpbw)
                     for linkdiscoveredname in linkslist:
                         if discoveredname == linkdiscoveredname: duplicatelink = True
                     if 'Ordered L1 Hops' in v3 and not duplicatelink:
@@ -72,76 +89,87 @@ def main():
                             linkslist.append(discoveredname)
                             c += 1
                             i += 1
-                            for l1hop in v3.get('Ordered L1 Hops'):
-                                nodelist = []
-                                firstlasthop = False
-                                for k5, v5 in l1hop.get('Nodes').items():
-                                    if k5 == firstnode or k5 == lastnode: firstlasthop = True
-                                    nodelist.append(k5)
-                                if not firstlasthop:
-                                    l1hops.append(nodelist)
-                                elif nodelist[0] == firstnode:
-                                    firstl1node = nodelist[1]
-                                elif nodelist[1] == firstnode:
-                                    firstl1node = nodelist[0]
-                                elif nodelist[0] == lastnode:
-                                    lastl1node = nodelist[1]
-                                elif nodelist[1] == lastnode:
-                                    lastl1node = nodelist[0]
+                            l1hops, firstl1node, lastl1node = getfirstlastl1node(v3.get('Ordered L1 Hops'), firstnode,
+                                                                                 lastnode)
+
                             name = "L1_circuit_" + str(c)
                             l1circuit = waecode.planbuild.generateL1circuit(plan, name, firstl1node, lastl1node, l1hops,
-                                                                            rsvpbw)
+                                                                            intfbw)
                             name = "L3_circuit_" + str(i)
                             l3circuit = waecode.planbuild.generateL3circuit(plan, name, firstnode, lastnode)
                             l3circuit.setL1Circuit(l1circuit)
                             l3circuit.setCapacity(l1circuit.getBandwidth())
+                            intfdict = l3circuit.getAllInterfaces()
+                            for k6, v6 in intfdict.items():
+                                v6.setResvBW(int(rsvpbw / 1000))
+
                     elif not duplicatelink:
                         i += 1
                         name = "L3_circuit_" + str(i)
                         linkslist.append(discoveredname)
                         l3circuit = waecode.planbuild.generateL3circuit(plan, name, firstnode, lastnode)
-                        l3circuit.setCapacity(rsvpbw)
+                        l3circuit.setCapacity(intfbw)
+                        intfdict = l3circuit.getAllInterfaces()
+                        for k6, v6 in intfdict.items():
+                            v6.setResvBW(int(rsvpbw / 1000))
 
                     duplicatelink = False
 
+    # Add LSPs to plan
+    l3nodeloopbacks = []
+    for k1, v1 in l3linksdict.items():
+        tmpnode = {k1: v1['Loopback Address']}
+        l3nodeloopbacks.append(tmpnode)
+
+    with open("jsonfiles/lsps.json", 'rb') as f:
+        lsps = json.load(f)
+        f.close()
+    waecode.planbuild.generate_lsps(plan, lsps, l3nodeloopbacks)
+
+    # Save the plan file
     plan.serializeToFileSystem('planfiles/test.pln')
+
+    # Script completed
     print "done"
+
+
+def getfirstlastl1node(orderedl1hops, firstnode, lastnode):
+    l1hops = []
+    firstl1node = ""
+    lastl1node = ""
+    for l1hop in orderedl1hops:
+        nodelist = []
+        firstlasthop = False
+        for k5, v5 in l1hop.get('Nodes').items():
+            if k5 == firstnode or k5 == lastnode: firstlasthop = True
+            nodelist.append(k5)
+        if not firstlasthop:
+            l1hops.append(nodelist)
+        elif nodelist[0] == firstnode:
+            firstl1node = nodelist[1]
+        elif nodelist[1] == firstnode:
+            firstl1node = nodelist[0]
+        elif nodelist[0] == lastnode:
+            lastl1node = nodelist[1]
+        elif nodelist[1] == lastnode:
+            lastl1node = nodelist[0]
+    return l1hops, firstl1node, lastl1node
+
+
+def getintfbw(rsvpbw):
+    intfbw = 0
+    if rsvpbw > 0 and rsvpbw <= 1000000:
+        intfbw = 1000
+    elif rsvpbw > 1000000 and rsvpbw <= 10000000:
+        intfbw = 10000
+    elif rsvpbw > 10000000 and rsvpbw <= 40000000:
+        intfbw = 40000
+    elif rsvpbw > 40000000 and rsvpbw <= 100000000:
+        intfbw = 100000
+    else:
+        print "Error determining interface bandwidth!!!"
+    return intfbw
 
 
 if __name__ == '__main__':
     main()
-
-
-
-    # intfAkey = InterfaceKey(name = 'FOO-L3-intf', sourceKey = nodeAKey)
-    # intfBkey = InterfaceKey(name='BAR-L3-intf', sourceKey=nodeBKey)
-    # circRec = CircuitRecord(name='L3-FOO-to-BAR',interfaceAKey=intfAkey,interfaceBKey=intfBkey)
-
-    # circuitManager = plan.getNetwork().getCircuitManager()
-    # circKey = circuitManager.newCircuitKey(intfAkey,intfBkey)
-    # circ = circuitManager.getCircuit(circKey)
-
-    # Check for the existence of a few nodes
-    # for nodeName in ['cr1.ams', 'cr7.ams', 'er1.lon', 'er1.nyc']:
-    #     nodeKey = com.cisco.wae.design.model.net.NodeKey(name=nodeName)
-    #     if plan.getNetwork().getNodeManager().hasNode(nodeKey):
-    #         print('Network contains node ' + nodeName)
-    #     else:
-    #         print('Network does not contain node ' + nodeName)
-
-    # l1nodekeys = l1NodeManager.getAllL1NodeKeys()
-    # l1nodeAKey = l1nodekeys[0]
-    # l1nodeBKey = l1nodekeys[1]
-
-    # l1nodeAKey = L1NodeKey('L1-FOO')
-    # l1nodeBKey = L1NodeKey('L1-BAR')
-
-
-
-    # l1NodeManager = plan.getNetwork().getL1Network().getL1NodeManager()
-    #
-    # l1nodeRec = L1NodeRecord(name="L1-FOO")
-    # l1NodeManager.newL1Node(l1nodeRec)
-    #
-    # l1nodeRec = L1NodeRecord(name="L1-BAR")
-    # l1NodeManager.newL1Node(l1nodeRec)
