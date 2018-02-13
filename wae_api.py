f5682a71a4da23518d59700fe97627a2c8de5b12
brfoster <brfoster@cisco.com>
2018-02-13 17:08:21 -0500

    Removed comments.

--- wae_api.py
+++ wae_api.py
@@ -49,20 +49,20 @@ def main():
     rootLogger.addHandler(consoleHandler)
 
     # # Delete all output files
-    # logging.info("Cleaning files from last collection...")
-    # try:
-    #     remove_tree('jsonfiles')
-    #     remove_tree('xmlgets')
-    # except Exception as err:
-    #     logging.info("No files to cleanup...")
-    #
-    # # Recreate output directories
-    # mkpath('jsonfiles')
-    # mkpath('xmlgets')
-    # mkpath(planfiles_root)
-    #
-    # # Run the collector...
-    # xmlcode.collect.runcollector(baseURL, epnmuser, epnmpassword)
+    logging.info("Cleaning files from last collection...")
+    try:
+        remove_tree('jsonfiles')
+        remove_tree('xmlgets')
+    except Exception as err:
+        logging.info("No files to cleanup...")
+
+    # Recreate output directories
+    mkpath('jsonfiles')
+    mkpath('xmlgets')
+    mkpath(planfiles_root)
+
+    # Run the collector...
+    xmlcode.collect.runcollector(baseURL, epnmuser, epnmpassword)
 
     # print "PYTHONPATH=" + os.getenv('PYTHONPATH')
     # print "PATH=" + os.getenv('PATH')
